1095f0e9a43739ec7a5732622acf89dcc828444e
Niya Maju <niyachu15@gmail.com>
2026-07-16 07:49:13 +0100

    set up connection

--- VPN for Office/VPNClient.java
+++ VPN for Office/VPNClient.java
@@ -1,1 +1,16 @@
-public class VPNClient{}
+
+import java.net.Socket;
+
+public class VPNClient{
+    public static void main(String[] args) {
+        try {
+
+            System.out.println("Client started");
+            Socket soc = new Socket("localhost", 9806);
+        } 
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
--- VPN for Office/VPNServer.java
+++ VPN for Office/VPNServer.java
@@ -1,1 +1,17 @@
-public class VPNServer{}
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class VPNServer{
+    public static void main(String[] args){
+        try {
+            System.out.println("Waiting for clients...");
+            ServerSocket ss = new ServerSocket(9806);
+            Socket soc = ss.accept();
+            System.out.println("Connection established");
+        } 
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
