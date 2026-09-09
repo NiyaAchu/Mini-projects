f7e7265fe44dd42d7d1d1fa60ef0834aff35fcfa
Niya Maju <niyachu15@gmail.com>
2026-09-09 21:20:15 +0100

    Created keyboardinputs and mouseinputs class

--- Platformer Game/PlatformerGame/src/main/GamePanel.java
+++ Platformer Game/PlatformerGame/src/main/GamePanel.java
@@ -1,5 +1,44 @@
 package main;
 
-public class GamePanel {
+import inputs.KeyboardInputs;
+import inputs.MouseInputs;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel{
+
+    private MouseInputs mouseInputs;
+    private int xDelta = 100, yDelta = 100;
+
+    public GamePanel() {
+
+        mouseInputs = new MouseInputs(this);
+        addKeyListener(new KeyboardInputs(this));
+        addMouseListener(mouseInputs);
+        addMouseMotionListener(mouseInputs);
+        
+    }
+
+    public void changeXDelta(int value){
+        this.xDelta += value;
+        repaint();
+    }
+
+    public void changeYDelta(int value){
+        this.yDelta += value;
+        repaint();
+    }
+
+    public void setRectPos(int x, int y){
+        this.xDelta = x;
+        this.yDelta = y;
+        repaint();
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        g.fillRect(xDelta, yDelta, 200, 50);
+    }
 
 }
