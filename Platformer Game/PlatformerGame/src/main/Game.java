88c80a045d73e373805dc1a86228f0678cd73e88
Niya Maju <niyachu15@gmail.com>
2026-09-18 13:10:29 +0100

    refined

--- Platformer Game/PlatformerGame/src/main/Game.java
+++ Platformer Game/PlatformerGame/src/main/Game.java
@@ -28,6 +28,9 @@ public class Game implements Runnable{
         double timePerFrame = 1000000000.0 / FPS_SET;
         long lastFrame = System.nanoTime();
         long now = System.nanoTime();
+        
+        int frames = 0;
+        long lastCheck = System.currentTimeMillis();
 
         while (true) { 
 
@@ -37,6 +40,13 @@ public class Game implements Runnable{
         
                 gamePanel.repaint();
                 lastFrame = now;
+                frames++;
+            }
+            
+            if (System.currentTimeMillis() - lastCheck >= 1000){
+                lastCheck = System.currentTimeMillis();
+                System.out.println("FPS: " + frames);
+                frames = 0;
             }
             
         }
