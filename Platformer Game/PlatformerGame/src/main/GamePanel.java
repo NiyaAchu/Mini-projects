06cc54e701bb6782f63fb78f0949b25cab4d6819
Niya Maju <niyachu15@gmail.com>
2026-09-18 10:49:24 +0100

    added a fps counter

--- Platformer Game/PlatformerGame/src/main/GamePanel.java
+++ Platformer Game/PlatformerGame/src/main/GamePanel.java
@@ -9,6 +9,8 @@ public class GamePanel extends JPanel{
 
     private MouseInputs mouseInputs;
     private int xDelta = 100, yDelta = 100;
+    private int frames= 0;
+    private long lastCheck = 0;
 
     public GamePanel() {
 
@@ -39,6 +41,13 @@ public class GamePanel extends JPanel{
         super.paintComponent(g);
 
         g.fillRect(xDelta, yDelta, 200, 50);
+
+        frames++;
+        if (System.currentTimeMillis() - lastCheck >= 1000){
+            lastCheck = System.currentTimeMillis();
+            System.out.println("FPS: " + frames);
+            frames = 0;
+        }
         repaint();
     }
 
