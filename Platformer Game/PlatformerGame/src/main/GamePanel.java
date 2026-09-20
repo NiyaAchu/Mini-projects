2dafca9428cc3c0cc7ff2a7e0ee772ed30961842
Niya Maju <niyachu15@gmail.com>
2026-09-20 18:10:28 +0100

    imported images yayy

--- Platformer Game/PlatformerGame/src/main/GamePanel.java
+++ Platformer Game/PlatformerGame/src/main/GamePanel.java
@@ -2,30 +2,42 @@ package main;
 
 import inputs.KeyboardInputs;
 import inputs.MouseInputs;
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.util.Random;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel{
 
     private MouseInputs mouseInputs;
     private float xDelta = 100, yDelta = 100;
-    private float xDir = 1f, yDir = 1f;
-    private int frames= 0;
-    private long lastCheck = 0;
-    private Color color = new Color(150, 20, 90);
-    private Random random;
+    private BufferedImage img;
+
 
     public GamePanel() {
 
-        random = new Random();
         mouseInputs = new MouseInputs(this);
+
+        importImg();
+
         setPanelSize();
         addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
         addMouseMotionListener(mouseInputs);
+        
+    }
+
+    private void importImg(){
+        InputStream is = getClass().getResourceAsStream("/player_sprites.png");
+
+        try {
+            img = ImageIO.read(is);            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         
     }
 
@@ -53,35 +65,10 @@ public class GamePanel extends JPanel{
     public void paintComponent(Graphics g){
         super.paintComponent(g);
 
-        updateRectangle();
-        g.setColor(color);
-        g.fillRect((int)xDelta, (int)yDelta, 200, 50);
-
+        g.drawImage(img, 0, 0, null);
 
     }
 
-    private void updateRectangle(){
-        xDelta+=xDir;
-        if (xDelta > 400 || xDelta < 0){
-            xDir *= -1;
-            color = getRndColor();
-        }
 
-        yDelta += yDir;
-        if(yDelta > 400 || yDelta < 0){
-            yDir *= -1;
-            color = getRndColor();
-        }
-
-    }
-
-    private Color getRndColor() {
-        int r = random.nextInt(255);
-        int g = random.nextInt(255);
-        int b = random.nextInt(255);
-
-        return new Color(r, g, b);
-
-    }
 
 }
